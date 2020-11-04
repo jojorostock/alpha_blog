8b14ca48f7da812df0f5e0eddcc9305631ffd791
Jojo Rostock <jojorostock@yahoo.com>
2020-11-04 21:25:03 +0000

    Clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
             render 'edit'
