e912c07ce5cfff2e7b334a4e106edd4e86762e1f
Jojo Rostock <jojorostock@yahoo.com>
2020-11-05 17:56:20 +0000

    create users action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,16 +24,17 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up" 
+      redirect_to articles_path
+      # format.html { redirect_to @user, notice: 'User was successfully created.' }
+      # format.json { render :show, status: :created, location: @user }
+    else
+      render 'new'
+      # format.html { render :new }
+      # format.json { render json: @user.errors, status: :unprocessable_entity }
     end
   end
 
@@ -69,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
