f894b22592c3c56626657e9231369d08f09a8b24
Jojo Rostock <jojorostock@yahoo.com>
2020-11-05 17:32:38 +0000

    working on signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#creat'
 end
